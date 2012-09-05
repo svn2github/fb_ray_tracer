6e6033ed57d5ebb251944705c279c6ac86bcc919
foreverbell <foreverbell@a142d196-329d-4e72-972a-086f8ffe8ca1>
2012-09-05 05:00:04 +0000

    Implemented: surface_disk.
    git-svn-id: http://foreverbell.0ginr.com/svn/ray_tracer@43 a142d196-329d-4e72-972a-086f8ffe8ca1

---
+++ ray_tracer/surface_disk.cpp
@@ -0,0 +1,40 @@
+
+#include <cmath>
+#include "misc.hpp"
+#include "ray.hpp"
+#include "surface_disk.hpp"
+
+namespace ray_tracer {
+
+	surface_disk::surface_disk() {
+		set_center(point3D());
+		set_normal(vector3D());
+		set_radius(0);
+	}
+
+	surface_disk::surface_disk(const point3D &center_, const vector3D &normal_, double radius) {
+		set_center(center_);
+		set_normal(normal_);
+		set_radius(radius);
+	}
+
+	bool surface_disk::hit(ray *ray_ptr, double tmin, hit_info *hit_ptr) const {
+		double deno = normal * ray_ptr->get_dir();
+		double t = (center - ray_ptr->get_origin()) * normal / deno;
+		point3D p;
+
+		if (dblcmp(deno) == 0 || t < tmin) {
+			return false;
+		}
+		p = ray_ptr->get_origin() + ray_ptr->get_dir() * t;
+		if ((center - p).length2() > radius2) {
+			return false;
+		}
+		hit_ptr->hit_t = t;
+		return true;
+	}
+
+	vector3D surface_disk::get_normal(const point3D &point) const {
+		return normal;
+	}
+}
---
+++ ray_tracer/surface_disk.hpp
@@ -0,0 +1,51 @@
+#ifndef __SURFACE_DISK_HPP
+#define __SURFACE_DISK_HPP
+
+#include "surface.hpp"
+
+namespace ray_tracer {
+	class surface_disk : public surface {
+	public:
+		surface_disk();
+		surface_disk(const point3D &, const vector3D &, double);
+		point3D get_center() const;
+		void set_center(const point3D &);
+		double get_radius() const;
+		void set_radius(const double &);
+		vector3D get_normal() const;
+		void set_normal(const vector3D &);
+		bool hit(ray *, double, hit_info *) const;
+		vector3D get_normal(const point3D &) const;
+	private:
+		point3D center;
+		vector3D normal;
+		double radius, radius2;
+	};
+
+	inline point3D surface_disk::get_center() const {
+		return center;
+	}
+	
+	inline void surface_disk::set_center(const point3D &center_) {
+		center = center_;
+	}
+
+	inline vector3D surface_disk::get_normal() const {
+		return normal;
+	}
+
+	inline void surface_disk::set_normal(const vector3D &normal_) {
+		normal = normal_.normalized();
+	}
+	
+	inline double surface_disk::get_radius() const {
+		return radius;
+	}
+
+	inline void surface_disk::set_radius(const double &radius_) {
+		radius = radius_;
+		radius2 = radius_ * radius_;
+	}
+}
+
+#endif
